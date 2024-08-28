485e21186f1f342a2a256c46ea2d3e99485b2332
SRIZ <135598332+srijalkapri@users.noreply.github.com>
2024-08-28 21:20:38 +0545

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+img_1 = cv2. imread("fruits.jpg",0)
+gamma = 2
+img_2 = np.power(img_1, gamma)
+gamma =0.3
+img_3 = np.power(img_1, gamma)
+gamma = 1.9
+img_4 = np.power(img_1 , gamma)
+
+
+
+plt.subplot(221)
+plt.imshow(img_1, cmap="gray")
+plt.title("original")
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(222)
+plt.imshow(img_2, cmap="gray")
+plt.title("Gamma-2")
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(223)
+plt.imshow(img_3, cmap="gray")
+plt.title("Gamma-3")
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(224)
+plt.imshow(img_4, cmap="gray")
+plt.title("Gamma-4")
+plt.xticks([]), plt.yticks([])
+
+plt.show()
+
